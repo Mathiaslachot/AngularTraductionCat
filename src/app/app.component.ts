f1e306e5e20ae6a03e7713454c45d33d37d43955
Mathias <mathias.lachot@orange.fr>
2020-02-11 19:42:07 +0100

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import CatTranslator from "../lib/translator.js";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'AngulatCat';
+export class AppComponent implements OnInit {
+  title = "AngulatCat";
+  messageArray: any = [
+    CatTranslator.translateText("Hi,beautiful cat.How are you?")
+  ];
+  testObject: any = {};
+  registerForm: FormGroup;
+  submitted = false;
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      message: ["", Validators.required]
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    const messageString = this.registerForm.value.message.toString();
+
+    this.messageArray.push(CatTranslator.translateText(messageString));
+
+    this.registerForm.reset();
+  }
+
+  onReset() {
+    this.submitted = false;
+    this.registerForm.reset();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { MessageComponent } from "./message/message.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MessageComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
